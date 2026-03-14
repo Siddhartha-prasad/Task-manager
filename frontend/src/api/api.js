5e03db3d167db3ff4a10cd75207feb60318a1819
Siddhartha-prasad <prasadsiddhartha2002@gmail.com>
2026-03-14 17:21:00 +0530

    Update api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API= axios.create({
-    baseURL: "https://task-manager-fa5e.onrender.com"
+    baseURL: "https://task-manager-fa5e.onrender.com/api"
 });
 
 export default API;
